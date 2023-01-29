cf78df5dc1a59ec80c3776015a09c42d690ec272
diszell2008 <listening2009@gmail.com>
2023-01-29 18:19:11 +0700

    Update v0.11.7

--- wormholes_install.sh
+++ wormholes_install.sh
@@ -69,7 +69,7 @@ if [ -n "$ky" ]; then
     fi
 fi         
 
-docker run -id -e KEY=$ky  -p 31303:30303 -p 8745:8545 -v /wm/.wormholes:/wm/.wormholes --name wormholes wormholestech/wormholes:v0.11.6
+docker run -id -e KEY=$ky  -p 31303:30303 -p 8745:8545 -v /wm/.wormholes:/wm/.wormholes --name wormholes wormholestech/wormholes:v0.11.7
 
 echo "Your private key:"
 sleep 5
